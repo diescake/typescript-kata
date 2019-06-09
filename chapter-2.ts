737f52e75af91cf9157b324730f8872decdc5563
Daisuke Kondo <daisuke.kondo@access-company.com>
2019-06-10 03:44:16 +0900

    Update chapter-2.ts

--- chapter-2.ts
+++ chapter-2.ts
@@ -20,3 +20,26 @@ type jsonType = typeof json
 const tuple = [false, 1, '2'] as const
 const a = 'A' as const
 let b = a
+
+// Extracting child's union types
+type TupleToUnion<T> = T extends (infer I)[] ? I : never
+type FruitsTuple = ['banana', 'apple', 'orange', number]
+type FruitsUnion = TupleToUnion<FruitsTuple>
+
+type MapToUnion<T> = T extends { [k: string]: infer I } ? I : never
+type FruitsMap = {
+  banana: 'banana',
+  apple: 'apple',
+  orange: 'orange',
+}
+
+type FruitsUnion2 = MapToUnion<FruitsMap>
+
+// Extracting a compatible type
+type FruitsMap2 = {
+  banana: { name: 'banana', price: number }
+  apple: { name: 'apple', price: number }
+  orange: { name: 'orange' }
+}
+
+// type PickedFruits = PickUp<FruitsMap, { price: numer }>
